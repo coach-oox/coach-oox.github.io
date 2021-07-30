afc5b35c42118e6e2d314711a14a414ba2dbaf8e
coachoox <coachmsoox@gmail.com>
2021-07-30 14:29:48 +0900

    update: 2021-07-30 autocomplete

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210729.2049';
+const cacheName = 'chirpy-20210730.1327';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
