da7cf34a5375452abbc6704251119947b358475c
dohaelee <dev.dohaelee@gmail.com>
2021-09-18 14:12:19 +0900

    update: 2021-09-18 autocomplete

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210913.0931';
+const cacheName = 'chirpy-20210918.1309';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
